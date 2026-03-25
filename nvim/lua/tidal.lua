0a4d38ffba9e1c99fc71c4e4dfec743bc22b95e7
Sven Schittecatte <sven@devlock.be>
2026-03-25 15:57:33 +0100

    Add explicit tidal.nvim keybindings for foot/tmux compatibility
    Shift+Enter and Alt+Enter don't pass through reliably in foot/tmux
    due to CSI-u keyboard protocol. Use leader-based keymaps instead:
      <leader>e  send line (normal) / send selection (visual)
      <leader>E  send block (multi-line)
      <leader>h  hush

--- nvim/lua/tidal.lua
+++ nvim/lua/tidal.lua
@@ -29,6 +29,24 @@ return {
 					split = "v",
 				},
 			})
+
+			-- Explicit keybindings that work reliably in foot/tmux
+			-- (Shift+Enter and Alt+Enter may not pass through correctly)
+			vim.keymap.set("n", "<leader>e", function()
+				require("tidal").send_line()
+			end, { desc = "Tidal: send line", ft = "tidal" })
+
+			vim.keymap.set("v", "<leader>e", function()
+				require("tidal").send_visual()
+			end, { desc = "Tidal: send selection", ft = "tidal" })
+
+			vim.keymap.set("n", "<leader>E", function()
+				require("tidal").send_block()
+			end, { desc = "Tidal: send block", ft = "tidal" })
+
+			vim.keymap.set("n", "<leader>h", function()
+				require("tidal").send_hush()
+			end, { desc = "Tidal: hush", ft = "tidal" })
 		end,
 	},
 }
